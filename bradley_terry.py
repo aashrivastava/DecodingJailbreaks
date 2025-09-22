ead3f6fa54d91670132eb43cc912db2954c2dd79
Aryan Shrivastava <114686168+aashrivastava@users.noreply.github.com>
2025-09-22 13:05:53 -0700

    anonymize bradley_terry script

--- bradley_terry.py
+++ bradley_terry.py
@@ -1,15 +1,3 @@
-'''
-THIS SCRIPT IS WHOLLY ATTRIBUTED TO:
-@article{lamparth2025movingmedicalexamquestions,
-  title={Moving Beyond Medical Exam Questions: A Clinician-Annotated Dataset of Real-World Tasks and Ambiguity in Mental Healthcare},
-  author={Lamparth, Max and Grabb, Declan and Franks, Amy and Gershan, Scott and Kunstman, Kaitlyn N. and Lulla, Aaron and Drummond Roots, Monika and Sharma, Manu and Shrivastava, Aryan and Vasan, Nina and Waickman, Colleen},
-  journal={arXiv preprint arXiv:2502.16051},
-  year={2025},
-  url={https://arxiv.org/abs/2502.16051}
-}
-
-Slight modifications to the original code were made to make it work with the new dataset structure.
-'''
 import argparse
 import pandas as pd
 import numpy as np
